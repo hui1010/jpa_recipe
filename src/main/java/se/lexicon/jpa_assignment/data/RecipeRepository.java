17352f489742a4e445a625e0f79939e854300bad
deltagare <huiyi.skarner@gmail.com>
2020-09-09 10:20:36 +0200

    Added a findAllByCategories_NameContainingIgnoreCase method, with parameter like String...names

--- src/main/java/se/lexicon/jpa_assignment/data/RecipeRepository.java
+++ src/main/java/se/lexicon/jpa_assignment/data/RecipeRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import se.lexicon.jpa_assignment.entity.Recipe;
-import java.util.Set;
+
 import java.util.List;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
@@ -14,9 +14,9 @@ public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
     //todo coming three methods do not work
     List<Recipe> findAllByRecipeIngredients_Ingredient_NameIgnoreCase(String ingredientName);
     List<Recipe> findAllByRecipeCategories_NameIgnoreCase(String categoryName);
-//    @Query("SELECT r FROM Recipe r JOIN RecipeCategory rc WHERE r. = :name ")
-//    List<Recipe> findAllByRecipeCategories_NameContainingIgnoreCase(@Param("name") String name, String ... names);
+//    @Query("SELECT r FROM Recipe r JOIN RecipeCategory rc WHERE rc.name = :name")
+//    List<Recipe> findAllByRecipeCategories_NameContainingIgnoreCase(@Param("name")String... names);
+    List<Recipe> findAllByRecipeCategories_NameContainingIgnoreCase(String...names);
 
-    //List<Recipe> findAllByRecipeIngredientsContainingIgnoreCase();
 
 }
